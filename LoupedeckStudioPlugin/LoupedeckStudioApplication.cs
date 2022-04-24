7fdcacd718a58225a1bf9ef5a8f3ac5cd557f751
Nicollas R <nicollasfernandesricas@gmail.com>
2022-04-23 23:35:13 -0400

    Initial commit

---
+++ LoupedeckStudioPlugin/LoupedeckStudioApplication.cs
@@ -0,0 +1,16 @@
+namespace Loupedeck.LoupedeckStudioPlugin
+{
+    using System;
+
+    public class LoupedeckStudioApplication : ClientApplication
+    {
+        public LoupedeckStudioApplication()
+        {
+
+        }
+
+        protected override String GetProcessName() => "";
+
+        protected override String GetBundleName() => "";
+    }
+}
